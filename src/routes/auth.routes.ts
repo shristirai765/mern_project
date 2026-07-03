b0cea059e22dec4b617544e0bcaa8e0009ab6497
Shristi Rai <shristirai.it@gmail.com>
2026-07-03 16:22:05 +0545

    add: auth routes

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { register } from "../controllers/auth.controller";
+
+const router = express.Router();
+
+//* register
+router.post("/register", register);
+
+//* login
+// router.post("/login", login);
+
+export default router;
