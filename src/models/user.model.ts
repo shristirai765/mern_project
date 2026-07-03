c11a56abfe5ee3c5352f216cf1de33c3d5bff809
Shristi Rai <shristirai.it@gmail.com>
2026-07-03 16:20:14 +0545

    add: user schema & model

---
+++ src/models/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    full_name:{
+        type: String,
+        required: [true, "full_name is required"],
+        minLength: [3, "name must be 3 character long"],
+        trim: true
+    },
+    email:{
+        type: String,
+        required: [true, "email is required"],
+        unique: [true, "user already exists with provided email"],
+        trim: true
+    },
+    password:{
+        type: String,
+        required: [true, "password is required"],
+    },
+    profile_image:{
+        type: String,
+    },
+    role:{
+        type: String,
+        enum: ["USER", "ADMIN", "SUPER ADMIN"],
+        default: "USER",
+    },
+    phone:{
+        type: String,
+        required: false,
+        maxLength: [10, "phone number at most be 10 ligits long"],
+    },
+},{timestamps: true});
+
+const User = mongoose.model("user", userSchema);
+export default User;
