551a151b9b8ca49cc3e5d6bbd8490c10831e0445
Shristi Rai <shristirai.it@gmail.com>
2026-07-08 14:43:52 +0545

    add: category crud

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,8 +1,115 @@
-//* get all
-//* getby id
+import { Request, Response, NextFunction } from "express";
+import Category from "../models/category.model";
+import { catchAsync } from "../utils/catchAsync.utils";
+import AppError from "../utils/appError.utils";
 
-//* create
 
-//* update
 
-//* delete- me
+//* get all -> sapana
+
+
+
+
+export const getAll = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const categories = await Category.find();
+
+    res.status(200).json({
+      success: true,
+      count: categories.length,
+      data: categories,
+    });
+  },
+);
+
+//* get by id  -> rubina
+
+export const getById = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const category = await Category.findById(id);
+
+    if (!category) {
+      throw new AppError("category not found", 404);
+    }
+
+    res.status(200).json({
+      message: `category by id ${id} is fetched`,
+      success: true,
+      status: "success",
+      data: category,
+    });
+  },
+);
+
+//* create  -> ashmita
+export const create = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name, description } = req.body;
+
+    if (!name) throw new AppError("name is required", 404);
+    if (!description) throw new AppError("description is required", 404);
+
+    const existingCategory = await Category.findOne({ name });
+    if (existingCategory) {
+      throw new AppError("name already exists", 404);
+    }
+
+    const category = new Category({ name, description });
+    await category.save();
+
+    res.status(201).json({
+      message: "Category created successfully",
+      status: "success",
+      success: true,
+      data: category,
+    });
+  },
+);
+
+//* update  -> atit
+
+export const update = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+
+    const existingCategory = await Category.findById({ id });
+
+    if (!existingCategory) {
+      throw new AppError("Brand does not exist", 404);
+    }
+
+    const { name, description } = req.body;
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      { _id: id },
+      { name, description },
+      { new: true },
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Brand updated successfully.",
+      data: updatedCategory,
+    });
+  },
+);
+
+//* delete  -> shristi
+export const remove = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const category = await Category.findByIdAndDelete({ _id: id });
+
+    if (!category) {
+      throw new AppError("category not found.", 404);
+    }
+
+    res.status(200).json({
+      message: "category deleted successfully.",
+      success: true,
+      status: "success",
+      data: null,
+    });
+  },
+);
