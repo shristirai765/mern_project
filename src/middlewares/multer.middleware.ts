58f37f86d2a283b8558a53b4f572fb960d018d00
Shristi Rai <shristirai.it@gmail.com>
2026-07-08 16:49:22 +0545

    add: multer middleware

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,2 +1,60 @@
+import multer, { FileFilterCallback } from "multer";
+import fs from "fs";
+import { Request } from "express";
+import AppError from "../utils/appError.utils";
+
+export const uploader = ()=>{
+    const folder = "uploads/";
+    const file_size = 5 * 1024 * 1024; //5mb
+
+    if(!fs.existsSync(folder)){
+        fs.mkdirSync(folder, {recursive: true});
+    }
+
+    //* multer disk storage
+    const storage = multer.diskStorage({
+        destination: (req, file, cb)=>{
+            const folder = 'uploads/';
+            cb(null, folder);
+        },
+        filename: (req, file, cb)=>{
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+            cb(null, file.fieldname + '-' + uniqueSuffix+ "-"+ file.originalname);
+        },
+    });
+
+    //* file filter
+    const fileFilter = (
+        req:Request, 
+        file: Express.Multer.File, 
+        callback: FileFilterCallback
+    )=>{
+        const allowed_mime_type = [
+            'image/jpg',
+            'image/png',
+            'image/jpeg',
+            'image/svg+xml',
+            'doc/pdf',
+
+        ];
+        if(!allowed_mime_type.includes(file.mimetype)){
+            callback(new AppError(`${file.mimetype} is not allowed`,  422));
+        }else{
+            callback(null, true);
+        }
+
+    };
 
 
+    //* multer upload instance
+    const upload = multer({
+        storage: storage,
+        fileFilter: fileFilter,
+         limits: {
+        fileSize: file_size,
+        
+    },
+});
+
+return upload;
+}
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,23 +1,11 @@
 import express from "express";
 import { register, login } from "../controllers/auth.controller";
-import multer from "multer";
+import { uploader } from "../middlewares/multer.middleware";
+
 
 const router = express.Router();
 
-//* multer disk storage
-const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        const folder = 'uploads/';
-        cb(null, folder);
-    },
-    filename: (req, file, cb)=>{
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix+ "-"+ file.originalname);
-    },
-});
-
-//* multer upload instance
-const upload = multer({storage: storage});
+const upload = uploader();
 
 //* register
 router.post("/register", upload.single("profile_image"), register);
