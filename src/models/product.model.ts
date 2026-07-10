0a7f5d6033848bb884207be8a27c68e0f9a0f887
Shristi Rai <shristirai.it@gmail.com>
2026-07-10 10:41:21 +0545

    add: product schema & model

---
+++ src/models/product.model.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import ImageSchema from "./image.model";
+
+
+const productSchema = new mongoose.Schema({
+    name:{
+        name: String,
+        required: [true, "name is required"],
+        trim: true,
+        minLength: 3,
+    },
+    brand:{
+        type: String,
+        required: [true, "brand is required"],
+    },
+    price:{
+        type: Number,
+        required: [true, "price is required"],
+    },
+    product_picture:{
+        type: ImageSchema,
+        required: [true, "product_picture is required"],
+    }
+
+},{timestamps: true}
+);
+
+
+const Product = mongoose.model("product", productSchema);
+export default Product;
