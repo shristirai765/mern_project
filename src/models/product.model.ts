9ef78d3649e43b868ed877974b8bec1dd5036b3d
Shristi Rai <shristirai.it@gmail.com>
2026-07-13 16:33:27 +0545

    add: product schema

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -9,18 +9,45 @@ const productSchema = new mongoose.Schema({
         trim: true,
         minLength: 3,
     },
-    brand:{
+    description:{
         type: String,
-        required: [true, "brand is required"],
+        required: [true, "description is required"],
+        minLength: 30,
+        maxLength: 500,
     },
     price:{
         type: Number,
         required: [true, "price is required"],
+        min: 0,
     },
-    product_picture:{
+    cover_image:{
         type: ImageSchema,
-        required: [true, "product_picture is required"],
-    }
+        required: [true, "cover image is required"],
+    },
+    // database store category: fkdslgdf=>{}
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, "category is required"],
+        ref: "category",
+    },
+    brand:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, "brand is required"],
+        ref: "brand",
+    },
+    images:[
+        {
+        type: ImageSchema,
+        default: null,
+    }],
+    new_arrival:{
+        type: Boolean,
+        default: true,
+    },
+    is_featured:{
+        type: Boolean,
+        default: false,
+    },
 
 },{timestamps: true}
 );
