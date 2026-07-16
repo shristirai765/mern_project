65836e8f8c5c998165e6d938a6522be3dd095111
Shristi Rai <shristirai.it@gmail.com>
2026-07-16 16:12:32 +0545

    add: wishlist model

--- src/models/wishList.model.ts
+++ src/models/wishList.model.ts
@@ -1,1 +1,19 @@
 //* user, product -id
+
+import mongoose from "mongoose";
+
+const wistListSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+    },
+    product:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "product"
+    },
+
+});
+
+const WishList = mongoose.model("wishlist", wistListSchema);
+
+export default WishList;
