94f8df4b15b0d72498bc4c0e6f4de7b8591b6a14
Shristi Rai <shristirai.it@gmail.com>
2026-07-19 16:10:12 +0545

    add: uncaught exception handler

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
+
+process.on("uncaughtException", (error)=>{
+    console.log("uncaughtException", error);
+    process.exit(1);
+});
+
+
 // import dotenv from "dotenv";
 import "dotenv/config";
 // import + call in a same line
-
 import app from "./app";
 import { connectDatabase } from "./config/db.config";
 import ENV_CONFIG from "./config/env.config";
+import mongoose from "mongoose";
 // import { verifyMailServerConnection } from "./config/nodemailer.config";
 // import { sendEmail } from "./utils/emailServer.utils";
 
@@ -12,12 +19,36 @@ const PORT = ENV_CONFIG.PORT;
 const DB_URI = ENV_CONFIG.DB_URI;
 // const DB_URL = 'mongodb+srv://';
 
+connectDatabase(DB_URI);
 
 //* listen
-app.listen(PORT, ()=>{
+const server = app.listen(PORT, ()=>{
     console.log(`Server is running at http://localhost:${PORT}`);
     // verifyMailServerConnection();
     // sendEmail();
 });
 
-connectDatabase(DB_URI);
+process.on("uncaughtException", (error)=>{
+    console.log("uncaughtException", error);
+    process.exit(1);
+});
+
+process.on("SIGINT", ()=>{
+    console.log("SIGINT");
+    server.close(async (error)=>{
+        console.log(error);
+        await mongoose.disconnect();
+        process.exit(0);
+
+    });
+});
+
+process.on("SIGTERM", ()=>{
+    console.log("SIGTERM");
+    server.close(async (error)=>{
+        console.log(error);
+        await mongoose.disconnect();
+        process.exit(0);
+
+    });
+});
